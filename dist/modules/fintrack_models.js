efb5e5270402b747d1adb17bd537c6c3667f43b5
mfaisalkemal <130155172+mfaisalkemal@users.noreply.github.com>
2023-08-12 01:19:15 +0700

    Add files via upload

---
+++ dist/modules/fintrack_models.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Fintrack = void 0;
+class Fintrack {
+    constructor(id, type, name, detail, amount) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.detail = detail;
+        this.amount = amount;
+    }
+}
+exports.Fintrack = Fintrack;
