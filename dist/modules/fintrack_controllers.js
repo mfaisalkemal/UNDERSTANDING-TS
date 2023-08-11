787265226cba71e6bff324e18e5b96c50b51c65c
mfaisalkemal <130155172+mfaisalkemal@users.noreply.github.com>
2023-08-12 01:27:56 +0700

    fix controllers

--- dist/modules/fintrack_controllers.js
+++ dist/modules/fintrack_controllers.js
@@ -1,14 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteFintrack = exports.updateFintrack = exports.getFintracks = exports.createFintrack = void 0;
+exports.deleteFintrack = exports.updateFintrack = exports.partialupdateFintrack = exports.getbyidFintracks = exports.getFintracks = exports.createFintrack = void 0;
 const fintrack_models_1 = require("./fintrack_models");
 const FINTRACKS = [];
+var counter = 0;
 const createFintrack = (req, res, next) => {
+    ++counter;
     const type = req.body.type;
     const name = req.body.name;
     const detail = req.body.detail;
     const amount = req.body.amount;
-    const newFintrack = new fintrack_models_1.Fintrack(Math.random().toString(), type, name, detail, amount);
+    const newFintrack = new fintrack_models_1.Fintrack(counter.toString(), type, name, detail, amount);
     FINTRACKS.push(newFintrack);
     res.status(201).json({ message: 'Created the Financial Tracking.', createdFintrack: newFintrack });
 };
@@ -17,6 +19,38 @@ const getFintracks = (req, res, next) => {
     res.json({ fintrack: FINTRACKS });
 };
 exports.getFintracks = getFintracks;
+const getbyidFintracks = (req, res, next) => {
+    const fintrackId = req.params.id;
+    const fintrackIndex = FINTRACKS.findIndex(fintrack => fintrack.id === fintrackId);
+    res.json(FINTRACKS[fintrackIndex]);
+};
+exports.getbyidFintracks = getbyidFintracks;
+const partialupdateFintrack = (req, res, next) => {
+    const fintrackId = req.params.id;
+    const fintrackIndex = FINTRACKS.findIndex(fintrack => fintrack.id === fintrackId);
+    var updatedType = req.body.type;
+    var updatedName = req.body.name;
+    var updatedDetail = req.body.detail;
+    var updatedAmount = req.body.amount;
+    if (updatedType == null) {
+        updatedType = FINTRACKS[fintrackIndex].type;
+    }
+    if (updatedName == null) {
+        updatedName = FINTRACKS[fintrackIndex].name;
+    }
+    if (updatedDetail == null) {
+        updatedDetail = FINTRACKS[fintrackIndex].detail;
+    }
+    if (updatedAmount == null) {
+        updatedAmount = FINTRACKS[fintrackIndex].amount;
+    }
+    if (fintrackIndex < 0) {
+        throw new Error('Could not find financial tracking!');
+    }
+    FINTRACKS[fintrackIndex] = new fintrack_models_1.Fintrack(FINTRACKS[fintrackIndex].id, updatedType, updatedName, updatedDetail, updatedAmount);
+    res.json({ message: 'Updated!', updatedFintrack: FINTRACKS[fintrackIndex] });
+};
+exports.partialupdateFintrack = partialupdateFintrack;
 const updateFintrack = (req, res, next) => {
     const fintrackId = req.params.id;
     const updatedType = req.body.type;
